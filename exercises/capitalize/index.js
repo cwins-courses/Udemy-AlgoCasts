ba331132ae3d6b056c144e5dc0e9985ec5d8fc9f
Charlie Winslow <cwinslow22@yahoo.com>
2019-03-04 18:13:35 -0600

    completed capitalize problem

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,23 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  let capd = "";
+  for (let char in str) {
+    // console.log(str[char])
+    if (str[char - 1] === " " || char == 0) {
+      capd += str[char].toUpperCase();
+    } else capd += str[char];
+  }
+  return capd;
+}
 
 module.exports = capitalize;
+
+// function capitalize(str) {
+//     let arr = [];
+//     for (let word of str.split(" ")) {
+//       arr.push(word[0].toUpperCase() + word.slice(1));
+//     }
+//     return arr.join(" ");
+//   }
