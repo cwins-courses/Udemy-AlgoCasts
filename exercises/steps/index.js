cccfc9bd19f580268556c7ecba1fb44195fe1999
Charlie Winslow <cwinslow22@yahoo.com>
2019-03-07 12:19:24 -0600

    implemented recursive solution for steps problem

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,17 +17,30 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  const hash = "#";
-  const space = " ";
-  for (let i = 1; i <= n; i++) {
-    let result = "";
-    result = hash.repeat(i) + space.repeat(n - i);
-    console.log(result);
+// recursive solution
+
+function steps(n, row = 0, stair = "") {
+  if (n == row) {
+    return;
   }
+
+  if (n === stair.length) {
+    console.log(stair);
+    steps(n, row + 1);
+    return;
+  }
+
+  if (stair.length <= row) {
+    stair += "#";
+  } else {
+    stair += " ";
+  }
+  steps(n, row, stair);
 }
 
 module.exports = steps;
+
+// My initial solution
 
 // function steps(n) {
 //     const hash = "#";
